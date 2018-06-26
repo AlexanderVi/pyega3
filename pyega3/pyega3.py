f7dca2734b2926a5d712069dacabc6c34446f242
alexanderv@ebi.ac.uk <alexanderv@ebi.ac.uk>
2018-06-26 15:28:30 +0100

    'ppp6'

--- pyega3/pyega3.py
+++ pyega3/pyega3.py
@@ -14,7 +14,7 @@ import hashlib
 import time
 
 debug = False
-version = "3.0.22"
+version = "3.0.23"
 
 def load_credentials(filepath):
     """Load credentials for EMBL/EBI EGA from specified file"""
